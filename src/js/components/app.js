0e71414e4ae83692645b3a88e84247acfe04349b
Maki Sun <themakisun@gmail.com>
2023-08-15 02:58:56 +0300

    Finished code

---
+++ src/js/components/app.js
@@ -0,0 +1,17 @@
+let user = {
+  name: 'Maki',
+  age: 18,
+  gender: 'male',
+  hobby: 'coding',
+};
+
+
+let content = [];
+
+for (let key of Object.keys(user)) {
+  content.push(user[key]);
+}
+
+console.log(content);
+
+
